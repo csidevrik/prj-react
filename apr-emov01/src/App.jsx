7e4077a210b3979c40585abe6002ab6fcf58c928
Carlos Sigua <carlos.sigua@gmail.com>
2023-07-16 12:30:10 -0500

    MATERIAL UI

---
+++ apr-emov01/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+import {Button, Container, Typography} from '@mui/material'
+function App() {
+  return (
+    <>
+    <Container>
+      <h2></h2>
+    </Container>
+    <Container>
+        <header className="App-header">
+          <Typography gutterBottom={true}>App fff </Typography>
+
+          <h1>App</h1>
+          <Button variant='text'>one button</Button>
+        </header>
+      </Container>
+    </>
+  );
+}
+
+export default App;
